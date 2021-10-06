a927c3d8892edb1fdd7bd6b1e1981d0557cf7a0a
khrastnig <victy@gmx.at>
2021-10-06 08:53:56 +0200

    datetime

--- hello.py
+++ hello.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+import datetime
+zeit=datetime.datetime.now()
 
 print("Guten Start is neue Schuljahr!")
 
 name=input("Bitte geben Sie ihren Benutzername an: ")
 print("Guten Start",name, "im neuen Schuljahr!")
 print(f'Guten Tag {name}!')
+
+print(zeit)
+
 
